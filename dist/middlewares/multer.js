5b5c9c8ed0537beb437957489e255f223e840d8a
Tabishdehlvi <tabishdehlvi3@gmail.com>
2024-05-24 14:55:14 +0500

    resolve image upload issue

--- dist/middlewares/multer.js
+++ dist/middlewares/multer.js
@@ -5,12 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.uploadUserPhoto = void 0;
 const multer_1 = __importDefault(require("multer"));
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage });
 const storage = multer_1.default.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "./public/temp/");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
+    filename: (req, file, cb) => {
+        console.log("file.mimetype", file.mimetype);
+        console.log("req", req);
+        console.log("file", file);
+        const ext = file.mimetype.split("/")[1];
+        cb(null, `user-${req.user._id}-${Date.now()}.${ext}`);
     },
 });
 const upload = (0, multer_1.default)({ storage });
--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,11 +1,25 @@
 import multer from "multer";
+import { AppError } from "../utils";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp/");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage });
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/temp/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
+  filename: (req, file, cb) => {
+    console.log("file.mimetype", file.mimetype);
+    console.log("req", req);
+    console.log("file", file);
+
+    const ext = file.mimetype.split("/")[1];
+    cb(null, `user-${req.user._id}-${Date.now()}.${ext}`);
   },
 });
 
