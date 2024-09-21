29fd14675a980fbb9a8b9e5d9ac705b61760526d
Tabish <thdevthree@gmail.com>
2024-09-21 15:39:41 +0500

    set own temPassword

--- dist/middlewares/generateRandomPasswordMiddleware.js
+++ dist/middlewares/generateRandomPasswordMiddleware.js
@@ -1,13 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generatePassword = void 0;
-const crypto_1 = __importDefault(require("crypto"));
 const generatePassword = (req, res, next) => {
-    const tempPassword = crypto_1.default.randomBytes(8 / 2).toString("hex");
-    req.body.password = tempPassword;
+    // const tempPassword = crypto.randomBytes(8 / 2).toString("hex");
+    // req.body.password = tempPassword;
+    req.body.password = "CollabDash@1";
     next();
 };
 exports.generatePassword = generatePassword;
