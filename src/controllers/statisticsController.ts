b20e8c9dcb937afe629f8b6149c72c549f9f7310
Tabish <thdevthree@gmail.com>
2024-09-26 16:35:11 +0500

    add total employees attribute in today attaendance availability

--- src/controllers/statisticsController.ts
+++ src/controllers/statisticsController.ts
@@ -193,30 +193,33 @@ export const allUserTodayAttendanceStatistics = catchAsync(
       new AppResponse(
         200,
         {
-          present: presentUserDetails.map((user) => ({
-            _id: user._id,
-            username: user.username,
-            full_name: user.full_name,
-            avatar: user.avatar,
-          })),
-          leave: leaveUsers.map((leave: any) => ({
-            _id: leave.user._id,
-            username: leave.user.username,
-            full_name: leave.user.full_name,
-            avatar: leave.user.avatar,
-          })),
-          absent: absentUserDetails.map((user) => ({
-            _id: user._id,
-            username: user.username,
-            full_name: user.full_name,
-            avatar: user.avatar,
-          })),
-          on_late: lateUserDetails.map((user) => ({
-            _id: user._id,
-            username: user.username,
-            full_name: user.full_name,
-            avatar: user.avatar,
-          })),
+          total_employees: totalEmployees.length,
+          today_availability: {
+            present: presentUserDetails.map((user) => ({
+              _id: user._id,
+              username: user.username,
+              full_name: user.full_name,
+              avatar: user.avatar,
+            })),
+            leave: leaveUsers.map((leave: any) => ({
+              _id: leave.user._id,
+              username: leave.user.username,
+              full_name: leave.user.full_name,
+              avatar: leave.user.avatar,
+            })),
+            absent: absentUserDetails.map((user) => ({
+              _id: user._id,
+              username: user.username,
+              full_name: user.full_name,
+              avatar: user.avatar,
+            })),
+            on_late: lateUserDetails.map((user) => ({
+              _id: user._id,
+              username: user.username,
+              full_name: user.full_name,
+              avatar: user.avatar,
+            })),
+          },
         },
         "",
         ResponseStatus.SUCCESS
